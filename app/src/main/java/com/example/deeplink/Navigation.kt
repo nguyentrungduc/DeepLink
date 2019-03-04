26ca8938cba0ee0991058e9e732cfdced66438fe
Admin <nguyenduc049@gmail.com>
2019-03-04 18:04:22 +0700

    navigation

--- app/src/main/java/com/example/deeplink/Navigation.kt
+++ app/src/main/java/com/example/deeplink/Navigation.kt
@@ -2,11 +2,13 @@ package com.example.deeplink
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Navigation : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
+        findViewById<TextView>(R.id.tv).text = NavigationArgs.fromBundle(intent.extras).xx
     }
 }
